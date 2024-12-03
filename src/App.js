799545fceff0d14d1f8130ffec6fa6ad4bbb0a2c
JoaoVictorFdeBarros <JoaoVictorFdeBarros@gmail.com>
2024-12-03 12:08:20 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useAuth } from "react-oidc-context";
 import './App.css';
+import { FaGithub } from "react-icons/fa";
 
 function App() {
+  const auth = useAuth();
+
+  if (auth.isLoading) {
+    return <div className="container">Loading...</div>;
+  }
+
+  if (auth.error) {
+    return (
+      <div className="container">
+        <div className="error-message">
+          Encountering error... {auth.error.message}
+        </div>
+      </div>
+    );
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div className="container">
+        <h1>Welcome</h1>
+        <div className="user-info">
+          <pre>Email: {auth.user?.profile.email}</pre>
+          <pre>ID Token: {auth.user?.id_token}</pre>
+          <pre>Access Token: {auth.user?.access_token}</pre>
+          <pre>Refresh Token: {auth.user?.refresh_token}</pre>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+            <div className="cognito-info">
+        <h2>Faça LogIn utilizando o Amazon Cognito</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Para acessar a aplicação você precisa estar logado, entre ou crie uma conta no botão abaixo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <button onClick={() => auth.signinRedirect()}>Entrar</button>
+      <div className="Footnote">
+        <a href='https://github.com/JoaoVictorFdeBarros/Cognito-login'><FaGithub /> Repositório</a>
+        <p>Desenvolvido por João Victor F. Barros - Dezembro 2024</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from "react-oidc-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_ze19WiWEX",
+  client_id: "fu82inout57l8q1q96beq5mgl",
+  redirect_uri: "https://master.d3ez8d1lrxyruu.amplifyapp.com/",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
